a529f344378d508e7d4f9d579866000315c76117
lucy-i <lucy@ilucy.in>
2019-09-29 13:01:00 -0700

    inital commit

---
+++ HOWTOBUILD.md
@@ -0,0 +1,3 @@
+"myLibrary:prod": "ng build @ilucy/theme && mkdir dist\ilucy\theme\assets && cp -R projects\ilucy\theme\src\assets\ dist\ilucy\theme\assets\ && npm run msgAssetsCopied"
+
+"msgAssetsCopied": "echo '📁 Assets Copied to Library Dist Folder 📁'",
---
+++ projects/ilucy/theme/src/lib/theme.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-theme',
+  template: `
+    <p>
+      theme works!
+    </p>
+  `,
+  styles: []
+})
+export class ThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
