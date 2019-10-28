78fe6e39f2cef215c6b571139205d9801a314b7e
lucy-i <lucy@ilucy.in>
2019-10-28 12:33:24 -0700

    added module for search components

---
+++ lib/ilucy/components/search/lib/search.component.d.ts
@@ -0,0 +1,5 @@
+import { OnInit } from '@angular/core';
+export declare class SearchComponent implements OnInit {
+    constructor();
+    ngOnInit(): void;
+}
---
+++ lib/ilucy/components/search/public-api.d.ts
@@ -0,0 +1,3 @@
+export * from './lib/search.service';
+export * from './lib/search.component';
+export * from './lib/search.module';
